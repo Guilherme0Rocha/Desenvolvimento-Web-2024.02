59f04a980c939eb98fe55781a4a9b33ae94afb14
Guilherme Resende da Rocha <202402075365@alunos.ibmec.edu.br>
2024-11-04 16:51:05 -0300

    .

--- dom/codigo.js
+++ dom/codigo.js
@@ -7,12 +7,31 @@ window.onload = function () {
         return dados;
     }
 
+    const trataClick = ( e ) => {
+        const artigo = e.currentTarget;
+
+        //cookie
+        document.cookie = `id=${artigo.dataset.id}`
+        document.cookie = `nome=${artigo.dataset.nome}`
+        
+        // session
+        sessionStorage.setItem('id', artigo.dataset.id);
+        sessionStorage.setItem('nome', artigo.dataset.nome);
+        sessionStorage.setItem('atleta', JSON.stringify(artigo.dataset));
+
+        //local
+        localStorage.setItem('id', artigo.dataset.id);
+
+        window.location = `outra.html?id=${artigo.dataset.id}`
+        
+
+    }
+
     const montaCartao = (atleta) => {
         const cartao = document.createElement('article');
         const nome = document.createElement('h1');
         const imagem = document.createElement('img');
         const desc = document.createElement('p');
-        const link = document.createElement('a');
 
         nome.innerHTML = atleta.nome;
         cartao.appendChild(nome);
@@ -23,23 +42,20 @@ window.onload = function () {
         desc.innerHTML = atleta.detalhes;
         cartao.appendChild(desc);
 
-        link.innerHTML = "Saiba Mais..."
+        /*link.innerHTML = "Saiba Mais..."
         link.href = `outra.html?id=${atleta.id}&altura=${atleta.altura}`
-        cartao.appendChild(link)
+        cartao.appendChild(link)*/
+
+        cartao.onclick = trataClick;
+
+        cartao.dataset.id = atleta.id;
+        cartao.dataset.nome = atleta.nome;
+        cartao.dataset.imagem = atleta.imagem;
 
         container.appendChild(cartao);
     }
-    
-    
-    /*`
-        <article class="card">
-        <h2 class="nunito">${dados.indexOf(atleta) + 1}/${dados.length}</h2>
-        <h1 class="nunito-bold">${atleta.nome}</h1>
-        <img src="${atleta.imagem}">
-        <p class="nunito">${atleta.detalhes}</p>
-        </article>
-    `;*/
-    pega_json("https://botafogo-atletas.mange.li/2024-1/masculino").then(
+
+    pega_json("https://botafogo-atletas.mange.li/2024-1/feminino").then(
         ( obj ) => {
         console.log(`isso imprime depois`);
         obj.forEach( (elemento) => montaCartao(elemento));
